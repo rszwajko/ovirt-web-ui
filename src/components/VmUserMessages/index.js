b245b27bebfdd4b491a061c63acdd314808d2876
Radoslaw Szwajkowski <rszwajko@redhat.com>
2022-01-31 17:45:14 +0100

    Temporarily commment out PF3 components
    Importing PatternFly 3 components triggers ca 30 errors of type:
    
    ERROR in ./node_modules/patternfly-react/dist/esm/components/Table/
    /Table.js 21:0-31 export 'actionHeaderCellFormatter' (imported as
    'Table') was not found in 'reactabular-table' (possible exports: ...
    
    All the errors seems related to the way Table component is packaged.

--- src/components/VmUserMessages/index.js
+++ src/components/VmUserMessages/index.js
@@ -1,34 +1,35 @@
-import React, { useContext, useState } from 'react'
+// import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { Notification, NotificationDrawer, MenuItem, Icon, Button } from 'patternfly-react'
+// import { Notification, NotificationDrawer, MenuItem, Icon, Button } from 'patternfly-react'
 
-import style from './style.css'
+// import style from './style.css'
 
 import { clearUserMessages, dismissEvent } from '_/actions'
-import { getFormatedDateTime, buildMessageFromRecord, toJS } from '_/helpers'
-import { MsgContext } from '_/intl'
+// import { getFormatedDateTime, buildMessageFromRecord, toJS } from '_/helpers'
+// import { MsgContext } from '_/intl'
 
 const UserMessage = ({ record, id, onDismissMessage }) => {
-  const { msg } = useContext(MsgContext)
-  const time = getFormatedDateTime(record.get('time'))
-  return (
-    <Notification seen>
-      <NotificationDrawer.Dropdown id={id}>
-        <MenuItem onClick={onDismissMessage}>
-          { msg.clear() }
-        </MenuItem>
-      </NotificationDrawer.Dropdown>
-      <Icon className='pull-left' type='pf' name='warning-triangle-o' />
-      <Notification.Content>
-        <Notification.Message>
-          { buildMessageFromRecord(record.toJS(), msg) }
-        </Notification.Message>
-        <Notification.Info leftText={time.date} rightText={time.time} />
-      </Notification.Content>
-    </Notification>
-  )
+  // const { msg } = useContext(MsgContext)
+  // const time = getFormatedDateTime(record.get('time'))
+  return null
+  // (
+  // <Notification seen>
+  //   <NotificationDrawer.Dropdown id={id}>
+  //     <MenuItem onClick={onDismissMessage}>
+  //       { msg.clear() }
+  //     </MenuItem>
+  //   </NotificationDrawer.Dropdown>
+  //   <Icon className='pull-left' type='pf' name='warning-triangle-o' />
+  //   <Notification.Content>
+  //     <Notification.Message>
+  //       { buildMessageFromRecord(record.toJS(), msg) }
+  //     </Notification.Message>
+  //     <Notification.Info leftText={time.date} rightText={time.time} />
+  //   </Notification.Content>
+  // </Notification>
+  // )
 }
 UserMessage.propTypes = {
   record: PropTypes.object.isRequired,
@@ -37,43 +38,44 @@ UserMessage.propTypes = {
 }
 
 const VmUserMessages = ({ userMessages, onClearMessages, onDismissMessage, onClose, show }) => {
-  const { msg } = useContext(MsgContext)
-  const [expanded, setExpanded] = useState(false)
+  // const { msg } = useContext(MsgContext)
+  // const [expanded, setExpanded] = useState(false)
 
-  const idPrefix = 'usermsgs'
+  // const idPrefix = 'usermsgs'
 
-  const messagesCount = userMessages.get('records').size
-  const messagesList = messagesCount
-    ? userMessages.get('records').map(r => (
-      <UserMessage
-        key={`msg-${r.get('time')}`}
-        record={r}
-        id={`${idPrefix}-msg-${r.get('time')}-dropdown`}
-        onDismissMessage={() => onDismissMessage(r.toJS())}
-      />
-    ))
-    : <NotificationDrawer.EmptyState title={msg.noMessages()} />
+  // const messagesCount = userMessages.get('records').size
+  // const messagesList = messagesCount
+  //   ? userMessages.get('records').map(r => (
+  //     <UserMessage
+  //       key={`msg-${r.get('time')}`}
+  //       record={r}
+  //       id={`${idPrefix}-msg-${r.get('time')}-dropdown`}
+  //       onDismissMessage={() => onDismissMessage(r.toJS())}
+  //     />
+  //   ))
+  // : <NotificationDrawer.EmptyState title={msg.noMessages()} />
 
-  return (
-    <NotificationDrawer hide={!show} expanded={expanded}>
-      <NotificationDrawer.Title title={msg.notifications()} onCloseClick={onClose} onExpandClick={() => setExpanded(!expanded)} />
-      <NotificationDrawer.PanelBody className={style['panel-body']}>
-        <div className={style['notifications-list']}>
-          {messagesList}
-        </div>
-        { messagesCount > 0 && (
-          <NotificationDrawer.PanelAction className={style['action-panel']}>
-            <NotificationDrawer.PanelActionLink data-toggle='clear-all'>
-              <Button bsStyle='link' onClick={() => onClearMessages(toJS(userMessages.get('records', [])))}>
-                <Icon type='pf' name='close' />
-                { msg.clearAll() }
-              </Button>
-            </NotificationDrawer.PanelActionLink>
-          </NotificationDrawer.PanelAction>
-        )}
-      </NotificationDrawer.PanelBody>
-    </NotificationDrawer>
-  )
+  return null
+  // (
+  //   <NotificationDrawer hide={!show} expanded={expanded}>
+  //     <NotificationDrawer.Title title={msg.notifications()} onCloseClick={onClose} onExpandClick={() => setExpanded(!expanded)} />
+  //     <NotificationDrawer.PanelBody className={style['panel-body']}>
+  //       <div className={style['notifications-list']}>
+  //         {messagesList}
+  //       </div>
+  //       { messagesCount > 0 && (
+  //         <NotificationDrawer.PanelAction className={style['action-panel']}>
+  //           <NotificationDrawer.PanelActionLink data-toggle='clear-all'>
+  //             <Button bsStyle='link' onClick={() => onClearMessages(toJS(userMessages.get('records', [])))}>
+  //               <Icon type='pf' name='close' />
+  //               { msg.clearAll() }
+  //             </Button>
+  //           </NotificationDrawer.PanelActionLink>
+  //         </NotificationDrawer.PanelAction>
+  //       )}
+  //     </NotificationDrawer.PanelBody>
+  //   </NotificationDrawer>
+  // )
 }
 VmUserMessages.propTypes = {
   userMessages: PropTypes.object.isRequired,
