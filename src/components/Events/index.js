e6c6f1c29265cdaf524de968f422b53c80dff804
Radoslaw Szwajkowski <rszwajko@redhat.com>
2022-05-18 17:00:24 +0200

    Add Events view
    Changes on the VM Details -> Overview Card:
    1. display last 2 events that are associated with the VM
    2. provide a "View All" link to a new sub-page: the Events view.
    
    Events view features:
    1. display events in a table with 3 columns:
      a) severity
      b) date (includes time)
      c) message
    2. allow filtering and sorting for all columns via toolbar
    3. limit the max number of events that can be fetched to 500
    4. do incremental refresh by fetching only new events

---
+++ src/components/Events/index.js
@@ -0,0 +1,4 @@
+export { default as EventsTable } from './EventsTable'
+export * from './EventStatus'
+export { default as EventFilters } from './EventFilters'
+export { default as EventSort } from './EventSort'
